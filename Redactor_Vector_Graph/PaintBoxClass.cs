8e53d6167234ecc9ce75393bd80470c787ef34cd
Moigh <45309205+Moigh@users.noreply.github.com>
2019-01-25 03:29:28 +1000

    Add files via upload
    Added Palet

---
+++ Redactor_Vector_Graph/PaintBoxClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Redactor_Vector_Graph {
+    public class PaintBox : Panel {
+        public PaintBox() {
+            this.DoubleBuffered = true;
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            UpdateStyles();
+        }
+    }
+}
